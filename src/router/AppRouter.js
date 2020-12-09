f6e9f3998a33fcc2bb3263ccbac112624fa99667
tcjohnosn2 <terrence.c.johnson2@gmail.com>
2020-12-08 22:18:55 -0600

    setting up components and figuring out redux

---
+++ src/router/AppRouter.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { connect} from 'react-redux'
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Login from '../components/Login';
+import Register from '../components/Register';
+
+export default class AppRouter extends Component {
+      render() {
+            return (
+                  <BrowserRouter>
+                  <div>
+                        <Switch>
+                              <Route 
+                              path="/" 
+                              component={Login} 
+                              exact={true}/>
+                              <Route path="/register"  
+                              component={Register}/>
+                        </Switch>
+                  </div>
+                  </BrowserRouter>
+            )
+      }
+}
