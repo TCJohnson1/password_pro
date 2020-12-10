a821623fae5b4b6c0a5899650a13e363f72f8c58
tcjohnosn2 <terrence.c.johnson2@gmail.com>
2020-12-10 13:16:58 -0600

    Started working on the login function

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
+import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
 
 export default class Footer extends Component {
       render() {
             return (
-                  <div>
-                        
+                  <Footer className="page-footer">
+                  <div className="container">
+                        <div className="row">
+                        <h5>About</h5>
+                        <p>
+                        Password Pro was created as a capstone project for General
+                        Assembly's Software Engineering Immersive program. Password Pro is a fullstack password management application created using Postgresql, Express, React and Nodejs
+                        </p>
+                        </div>
+                        <div>
+                        <h5>Contact</h5>
+                        <p>
+                        <a href="https://www.linkedin.com/in/terrence-c-johnson2/">
+                        Terrence Johnson
+                        </a>
+                        </p>
+                        </div>
                   </div>
+                  </Footer>
             )
       }
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,49 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { validateFields } from '../utilities/validation';
+import { Link } from 'react-router-dom'
+
 
 export default class Login extends Component {
+      state = {
+            email: '',
+            password: '',
+            errorMessage: ''
+      }
+      handleLogin = event => {
+            event.preventDefault()
+            const {email, password} = this.state
+            const fieldsToValidate = [{email}, {password}]
+
+            const allFieldsEntered = validateFields(fieldsToValidate)
+            if (!allFieldsEntered){
+                  this.setState({
+                        errorMessage: {
+                              signInError: 'Please enter all fields'
+                        }
+                  }) //Error is return for failure to enter fields
+            }else {
+                  this.setState({
+                        errorMessage: {
+                              signInError: ''
+                        }
+                  }) //Empty return for a successful login
+            }
+      }
+
+      handleChange = (event) => {
+            const { name, value} = event.target
+
+            this.setState ({
+                  [name]: value
+            })
+      }
+
       render() {
+            const {errorMessage} = this.state;
             return (
                   <div>
-                        <h1>Log In</h1>
+                        <h1>Password Pro</h1>
                   </div>
             )
       }
