05af49cd3447a9b3377f0c628ea3911579c9e1b9
tcjohnosn2 <terrence.c.johnson2@gmail.com>
2020-12-11 11:24:59 -0500

    starting registration component. Established state and fields to validate

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { validateFields } from '../utilities/validation'
+import { Link } from 'react-router-dom'
 
 export default class Register extends Component {
+      state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+            loginMessage: '',
+            errorMessage: '',
+            isSubmitted: false
+      }
+
+      registerUser = (event) => {
+            event.preventDefault();
+            const { firstName, lastName, email, password,confirmPassword } = this.state
+
+            const fieldsToValidate = [
+                  {firstName},
+                  {lastName},
+                  {email},
+                  {password},
+                  {confirmPassword}
+            ]
+      }
+
       render() {
             return (
                   <div>
